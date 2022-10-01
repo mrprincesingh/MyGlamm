dba03758bfeb213e9796173e7af59587a0e16f10
Dipali Sarjare <rushalisarjare1@gmail.com>
2022-10-01 23:28:28 +0530

    Conflicts resloved

---
+++ banker.js
@@ -0,0 +1,4 @@
+import footer from "./componants/footer.js";
+document.getElementById("footer").innerHTML=footer();
+import navbar from "./componants/navbar.js";
+document.getElementById("navbar").innerHTML=navbar();
---
+++ componants/navbar.js
@@ -0,0 +1,289 @@
+let navbar=()=>{
+    return `<div class="navchild1">
+    <div>
+     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="mr-2 w-4 h-4"><defs>
+        <linearGradient id="gift_svg__a" x1="157.079%" x2="50%" y1="-113.692%" y2="100%">
+        <stop offset="0%" stop-color="#FFC8C8"></stop>
+        <stop offset="100%" stop-color="#FF9797"></stop></linearGradient>
+        <linearGradient id="gift_svg__b" x1="197.914%" x2="50%" y1="18.377%" y2="59.659%"><stop offset="0%" stop-color="#FFC8C8"></stop>
+         <stop offset="100%" stop-color="#FF9797"></stop></linearGradient></defs><g fill="none"><path fill="url(#gift_svg__a)" d="M1 8.05v6.044c0 .556.449 1.007 1 1.007h5V8.05H1z"></path>
+        <path fill="url(#gift_svg__b)" d="M15 4.02h-3.164c.227-.156.422-.311.558-.45a2.117 2.117 0 0 0 0-2.964c-.784-.796-2.15-.794-2.933 0-.434.438-1.584 2.222-1.425 3.414h-.072c.158-1.192-.992-2.976-1.425-3.414-.784-.794-2.15-.794-2.933 0a2.12 2.12 0 0 0-.001 2.964c.137.139.332.294.559.45H1c-.551 0-1 .452-1 1.007v1.511c0 .278.224.504.5.504H7V5.027h2v2.015h6.5c.276 0 .5-.226.5-.504v-1.51c0-.556-.448-1.008-1-1.008zm-8.059-.033s-.042.033-.185.033c-.691 0-2.01-.723-2.441-1.16a1.105 1.105 0 0 1 0-1.544 1.06 1.06 0 0 1 1.515 0c.674.683 1.344 2.428 1.111 2.67zm2.302.033c-.142 0-.184-.032-.184-.033-.233-.243.437-1.988 1.111-2.67a1.083 1.083 0 0 1 1.515 0c.42.424.42 1.117 0 1.542-.431.438-1.75 1.16-2.442 1.16z"></path>
+        <path fill="url(#gift_svg__a)" d="M9 8.05v7.05h5c.552 0 1-.45 1-1.007V8.05H9z"></path></g>
+    </svg>
+    <a href="#"><p>Pick Any Product at Rs 199</p></a>
+  <a href=""><p style="color: #ff9797;">SHOP NOW</p></a>
+    </div>
+    <div>
+        <img src="https://img.icons8.com/pix/2x/experimental-home-pix.png" alt="">
+        <a  href="#"><p>MyGlamm Store</p></a>
+    </div>
+</div>
+<div class="navchild2">
+    <div>
+      <a href="./index.html"> <img src="https://files.myglamm.com/site-images/original/MyGlamm-Logo_1.jpg" alt="" ></a>
+       <input type="text"   placeholder= "Find Lipstick, Eyeliner, Makeup Tutorial, etc"> 
+    </div>
+    <div>
+        <a href="" ><i style="font-size:24px" class="fa">&#xf07a;</i></a>
+         <a href=""><i style='font-size:24px' class='fas'>&#xf406;</i></a> 
+    </div>
+</div>
+
+<div id="navchild3">
+    <div id="home">
+        <a href="./index.html">HOME</a>
+        <!-- <div id="subMenu">
+            <ul>
+                <div class="categories">
+                  
+                </div>
+            </ul>
+        </div> -->
+    </div>
+        <div id="makeup">
+            <a href="#">MAKEUP</a>
+            <div id="subMenu">
+                <ul>
+                    <div class="categories">
+                        <h3>LIPS</h3>
+                        <a href="#"> <p>Lipstick</p></a>
+                        <a href="#"> <p>Crayon Lipstick</p></a>
+                        <a href="#"> <p>Matte Lipstick</p></a>
+                        <a href="#"> <p>Hi-Shine Lipstick</p></a>
+                        <a href="#"> <p>Liquid Lipstick</p></a>
+                        <a href="#"> <p>Lip Gloss</p></a>
+                        <a href="#"> <p>Hi-Shine Lipgloss</p></a>
+                        <a href="#"> <p>Lip Liner</p></a>
+                        <a href="#"> <p>Lip Balm</p></a>
+                        <a href="#"> <p>Glitter</p></a>
+                        <a href="#"> <p>Lip Stain</p></a>
+                        <a href="#"> <p>Lipstick Set</p></a>
+                        <a href="#"> <p>Lip Kit</p></a>
+                        <a href="#"> <p>Find Your Lipstick</p></a>
+                    </div>
+                    <div class="categories">
+                        <h3>FACE</h3>
+                        <a href="#"> <p>Face Sets</p></a>
+                        <a href="#"> <p>Fixing Powder</p></a>
+                        <a href="#"> <p>Primer</p></a>
+                        <a href="#"> <p>Concealer</p></a>
+                        <a href="#"> <p>Foundation</p></a>
+                        <a href="#"> <p>Compact</p></a>
+                        <a href="#"> <p>Banana Powder</p></a>
+                        <a href="#"> <p>Highlighter</p></a>
+                        <a href="#"> <p>Blush</p></a>
+                        <a href="#"> <p>Bronzer</p></a>
+                        <a href="#"> <p>Sindoor</p></a>
+                        <a href="#"> <p>Cheek Stain</p></a>
+                        <a href="#"> <p>Setting Powder</p></a>
+                        <a href="#"> <p>Makeup Accessories</p></a>
+                        <a href="#"> <p>Makeup Brushes</p></a>
+                    </div>
+                    <div class="categories">
+                        <h3>EYES</h3>
+                            <a href="#"> <p>Eye Shadow</p></a>
+                            <a href="#"> <p>Eyeliner</p></a>
+                            <a href="#"> <p>Eyebrows</p></a>
+                            <a href="#"> <p>Glitter</p></a>
+                            <a href="#"> <p>Mascara</p></a>
+                            <a href="#"> <p>Kajal</p></a>
+                            <a href="#"> <p>Eyeshadow Brushes</p></a>
+                            <a href="#"> <p>Eye Combos</p></a>
+                    </div>
+                    <div class="categories">
+                        <h3>NAILS</h3>
+                        <a href="#"> <p>Nail Polish</p></a>
+                        <a href="#"> <p>Matte Nail Polish</p></a>
+                        <a href="#"> <p> Nail Lacquer</p></a>
+
+                        <h3>KITS AND COMBOS</h3>
+                        <a href=""><p>Makeup Kits</p></a>
+                        <a href=""><p>Bridal Makeup Kits</p></a>
+                        <a href=""><p>Makeup Combo</p></a>
+                        <a href=""><p>Makeup Gift Sets</p></a>
+                        <a href=""><p>Cosmetics Kits &  Combos</p></a>
+
+                        <h3>MAKEUP ACCESSORIES</h3>
+                        <a href=""><p>Makeup Brushes</p></a>
+                        <a href=""><p>Eyeshadow Brushes</p></a>
+                    </div>
+                </ul>
+            </div>
+        </div>
+
+
+    <div id="haircare">
+        <a href="">HAIR CARE</a>
+        <div id="subMenu">
+            <ul>
+                
+                <div class="categories">
+                    <h3>HAIR CARE</h3>
+                   <a href=""><p>Shampoo</p></a>
+                   <a href=""><p>Conditioner</p></a>
+                   <a href=""><p>Hair Oil</p></a>
+                   <a href=""><p>Serum</p></a>
+                   <a href=""><p>Hair Mask</p></a>
+                   <a href=""><p>Combos</p></a>
+                </div>
+                <div class="categories">
+                    <img src="https://files.myglamm.com/site-images/400x400/Artboard-1-(1).jpg" alt="">
+                    <h4>MYGLAMM SUPERFOODS ONION &...</h4>
+                    <p>For Hair Fall Control</p>
+                    <p>₹ 164</p>
+                </div>
+                <div class="categories">
+                    <img src="https://files.myglamm.com/site-images/400x400/Artboard-1-(1).jpg" alt="">
+                    <h4>MYGLAMM SUPERFOODS ONION &...</h4>
+                    <p>For Hair Fall Control</p>
+                    <p>₹ 264</p>
+                </div>
+                <div class="categories">
+                    <img src="https://files.myglamm.com/site-images/400x400/Artboard-1-(1).jpg" alt="">
+                    <h4>MYGLAMM SUPERFOODS ONION &..</h4>
+                    <p>For Hair Fall Control</p>
+                    <p>₹ 174</p>
+                </div>
+                <div class="categories">
+                    <img src="https://files.myglamm.com/site-images/400x400/Artboard-1-(1).jpg" alt="">
+                    <h4>MYGLAMM SUPERFOODS ONION & ...</h4>
+                    <p>For Hair Fall Control</p>
+                    <p>₹ 194</p>
+                </div>
+            </ul>
+        </div>
+    </div>
+
+    <div id="skincare">
+        <a href="">SKINCARE</a>
+        <div id="subMenu">
+            <ul>
+                <div class="categories">
+                    <a href=""><p>Moisturiser</p></a>
+                    <a href=""><p>Night Cream</p></a>
+                    <a href=""><p>Eye Cream</p></a>
+                    <a href=""><p>Body lotion</p></a>
+                    <a href=""><p>Serum</p></a>
+                    <a href=""><p>Sunscreen</p></a>
+                    <a href=""><p>Skincare Kits</p></a>
+                    <h3> CLEANSING PRODUCTS</h3>
+                    <a href=""><p>Exfoliator & <br> Scrub</p></a>
+                    <a href=""><p>Face Wash</p></a>
+                    <a href=""><p>Cleanser</p></a>
+                    <h3>MASK</h3>
+                    <a href=""><p>Face Pack</p></a>
+                    <a href=""><p>Sheet Mask</p></a>
+                </div>
+            </ul>
+        </div>
+    </div>
+
+    <div id="sanitizing">
+        <a href="">SANITIZING CARE</a>
+        <div id="subMenu">
+            <ul>
+                <div class="categories">
+                    <h3>SANITIZING CARE</h3>
+                    <a href=""><p>Sanitizing Wipes</p></a>
+                    <a href=""><p>Sanitizing Kits</p></a>
+                   <a href=""><p>Sanitizing Spray</p></a>
+                   <a href=""><p>Hand Sanitizer</p></a>
+                </div>
+                <div class="categories">
+                    <img src="https://files.myglamm.com/site-images/400x400/Germ-Shield_2.jpg" alt="">
+                    <h4>GERM SHIELD</h4>
+                    <p>25 Sanitizing Wipes + 1 Moisturizing Hand Sanitizer</p>
+                    <p>₹ 241</p>
+                </div>
+            </ul>
+        </div>
+    </div>
+
+    <div id="collection">
+        <a href="">COLLECTION</a>
+        <div id="subMenu">
+            <ul>
+                <div class="categories">
+                    <h3>SHOP BY COLLECTION</h3>
+                    <a href=""><p>MyGlamm</p></a>
+                    <a href=""><p>SUPERFOODS</p></a>
+                    <a href=""><p>Treat Love Care</p></a>
+                    <a href=""><p>Clean Beauty</p></a>
+                    <a href=""><p>POPxo</p></a>
+                    <a href=""><p>WIPEOUT</p></a>
+                    <a href=""><p>GLOW Iridescent Brightening Skincare</p></a>
+                    <a href=""><p>Manish Malhotra</p></a>
+                    <a href=""><p>YOUTHFUL</p></a>
+                    <a href=""><p>LIT Collection</p></a>
+                    <a href=""><p>POSE</p></a>
+                    <a href=""><p>K. Play</p></a>
+                    <a href=""><p>Just Dropped</p></a>
+                </div>
+                <div class="categories">
+                    <img src="https://files.myglamm.com/site-images/800x800/Soiree-(1).jpg" alt="">
+                    <h4>MANISH MALHOTRA 9 IN 1 EYESHADOW PALETTE - SOIRÉE</h4>
+                    <p>Compact Eyeshadow Palette with 9 shades in Matte, Metallic & Foil Finishes</p>
+                    <p>₹ 1850</p>
+                </div>
+                <div class="categories">
+                    <img src="https://files.myglamm.com/site-images/400x400/GGM-(1).jpg" alt="">
+                    <h4>GLOW TO GLAMOUR SHIMMER AND FIXING POWDER</h4>
+                    <p>2 in 1 Shimmer and Fixing Powder</p>
+                    <p>₹ 478</p>
+                </div>
+                <div class="categories">
+                    <img src="https://files.myglamm.com/site-images/400x400/14-Nail-Lacquer-metallic-Post-1-Sheer-Glitz.jpg" alt="">
+                    <h4>MANISH MALHOTRA NAIL LACQUER - SHEER GLITZ</h4>
+                    <p>Hi-Shine Gel Finish Nail Enamel | Made in Italy</p>
+                    <p>₹ 270 </p>
+                </div>
+                <div class="categories">
+                    <img src="https://files.myglamm.com/site-images/800x800/Tom-Collins_6.jpg" alt="">
+                    <h4>MYGLAMM LIT CREAMY MATTE LIPSTICK - TOM COLLINS</h4>
+                    <p>Precision Perfect Matte Slimstick</p>
+                    <p>₹ 419</p>
+                </div>
+            </ul>
+        </div>
+    </div>
+
+    <div id="reward">
+        <a href="">REWARDS</a>
+        <div id="subMenu">
+            <ul>
+                <div class="categories">
+                  
+                </div>
+            </ul>
+        </div>
+    </div>
+     
+    <div id="studio">
+        <a href="">MYGLAMM STUDIO</a>
+        <div id="subMenu">
+            <ul>
+                <div class="categories">
+                    <h3>STUDIOS</h3>
+                    <a href=""><p>BLOGS</p></a>
+                </div>
+            </ul>
+        </div>
+    </div>
+
+    <div id="offers">
+        <a href="">OFFERS</a>
+        <div id="subMenu">
+            <ul>
+                <div class="categories">
+                    <h3></h3>
+                </div>
+            </ul>
+        </div>
+    </div>
+
+</div>
+<hr>
+`
+};
+
+export default navbar;
